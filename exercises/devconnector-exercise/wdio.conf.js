0a0bdcb5b8e1fdd0f9cbec6d5c7cf96918186187
Jeanie Ma <jeanie.ma@GL07329M.local>
2020-06-02 23:19:04 +0100

    add report

--- exercises/devconnector-exercise/wdio.conf.js
+++ exercises/devconnector-exercise/wdio.conf.js
@@ -153,8 +153,10 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    // beforeSession: function (config, capabilities, specs) {
-    // },
+    beforeSession: function (config, capabilities, specs) {
+        const del = require('del');
+        del(['allure-report','allure-results','errorShots'])
+    },
     /**
      * Gets executed before test execution begins. At this point you can access to all global
      * variables like `browser`. It is the perfect place to define custom commands.
@@ -246,6 +248,8 @@ exports.config = {
      * @param {Array.<String>} specs List of spec file paths that ran
      */
     // after: function (result, capabilities, specs) {
+    //     var name = 'ERROR-chrome-' + Date.now();
+    //     browser.saveScreenshot('./errorShots/' + name + '.png');
     // },
     /**
      * Gets executed right after terminating the webdriver session.
